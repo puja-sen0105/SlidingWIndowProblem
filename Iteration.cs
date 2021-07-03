3b5e2cfe4efbe9254369dc21fe5ca92b71b7ca68
Sen, Puja <puja.sen@cgi.com>
2021-07-03 11:25:14 +0530

    Add project files.

---
+++ Iteration.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SlidingWindowProblem
+{
+    class SingleIteration
+    {
+        public static double[] AverageSum(int k, int[] arr)
+        {
+            int windowStart = 0;
+            double windowSum = 0;
+            double[] result = new double[arr.Length - k + 1]; // Number of subgroups
+
+            for (int windowEnd = 0; windowEnd < arr.Length; windowEnd++)
+            {
+                windowSum += arr[windowEnd];
+                if (windowEnd >= k - 1)
+                {
+                    result[windowStart] = windowSum / k;
+                    windowSum = windowSum - arr[windowStart];
+                    windowStart += 1;
+                }
+            }
+
+            return result;
+        }
+    }
+
+    class DoubleIteration
+    {
+        public static double[] AverageSum(int k, int[] arr)
+        {
+            double[] result = new double[arr.Length - k + 1]; // Number of subgroups
+
+            for (int i = 0; i <= arr.Length - k; i++)
+            {
+                double sum = 0;
+                for (int j = i; j < i + k; j++)
+                {
+                    sum += arr[j];
+                }
+                result[i] = sum / k;
+            }
+            return result;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace SlidingWindowProblem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double[] result = SingleIteration.AverageSum(5, new int[] { 1, 3, 2, 6, -1, 4, 1, 8, 2 });
+            double[] result1 = DoubleIteration.AverageSum(5, new int[] { 1, 3, 2, 6, -1, 4, 1, 8, 2 });
+            Console.WriteLine("Hello World!");
+            Console.ReadLine();
+        }     
+    }
+}
